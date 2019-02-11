2540b582dcb0d7ea795feae081dc0a72faf30537
monikapuk <pukmonika98@gmail.com>
2019-02-11 20:23:48 +0100

    add typing without the sound

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,21 @@
 "use strict";
+
+let fullText = document.querySelector('#typewriter').textContent;
+
+document.querySelector('#typewriter').textContent ="";
+let counter=0;
+
+console.log('fulltext: ', fullText)
+
+
+
+setInterval(() => {
+    if (counter < fullText.length){
+        document.querySelector('#typewriter').textContent = fullText.substring(0, counter + 1);
+    }
+    else{
+        counter = 0; 
+    }
+    counter++;
+}, 300);
+
