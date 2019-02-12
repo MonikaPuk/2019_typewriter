598f55e882e6213901818ea0f7a93f12e2a6bd3d
monikapuk <pukmonika98@gmail.com>
2019-02-12 09:03:19 +0100

    sounds

--- typewriter.js
+++ typewriter.js
@@ -1,21 +1,56 @@
 "use strict";
+const typeSoundOne = document.getElementById('typekey1');
+const typeSoundTwo = document.getElementById('typekey2');
+const typeSoundSpace = document.getElementById('typespace');
+;
 
 let fullText = document.querySelector('#typewriter').textContent;
 
 document.querySelector('#typewriter').textContent ="";
-let counter=0;
+
+let counter = 0;
 
 console.log('fulltext: ', fullText)
 
+const btn = document.getElementById('btn').addEventListener('click', init);
 
+function init(){
 
 setInterval(() => {
     if (counter < fullText.length){
         document.querySelector('#typewriter').textContent = fullText.substring(0, counter + 1);
+        if (fullText[counter] === " "){
+            playSoundSpace();
+        }
+        else{
+            playSound();
+        }
     }
     else{
         counter = 0; 
     }
     counter++;
-}, 300);
+}, 400);
+}
+
+
+function playSoundSpace() {
+    typeSoundSpace.play();
+}
+
+function playSound() {
+    let x = randomIntBetween(1,2);
+    if( x==1 ){
+        typeSoundOne.play();
+        console.log('1');
+    } else {
+        console.log('2');
+        typeSoundTwo.play();
+    }
+}
+
+
+function randomIntBetween(min,max) {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    }
 
